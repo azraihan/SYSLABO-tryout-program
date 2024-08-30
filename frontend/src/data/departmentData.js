c02e64ab54dca925c2ac90122e57ac2815c5f365
azraihan <113767690+azraihan@users.noreply.github.com>
2024-08-30 16:00:37 +0600

    Add files via upload

---
+++ frontend/src/data/departmentData.js
@@ -0,0 +1,254 @@
+export const departmentData = [
+  {
+    id: "24500",
+    sys_id: "f50e048147b10610d1f9cc39116d43f9",
+    name: "IT Support Division",
+    parent: "",
+    department_head: ["Kuniyoshi Terunuma"],
+    description: "Provides comprehensive IT support across the company.",
+    primary_contact: "Kuniyoshi Terunuma",
+    members: ["Haruka Tanaka", "Shota Yamada"],
+    expanded: false
+  },
+  {
+    id: "24510",
+    sys_id: "072656b147d102d10d1f9cc39116d4322",
+    name: "IT Support Division Purchasing Procurement Department",
+    parent: "24500",
+    department_head: ["Kuniyoshi Terunuma"],
+    description: "Responsible for procurement of IT equipment and services.",
+    primary_contact: "Ryuhei Masuda",
+    members: ["Natsuki Suzuki", "Hiroki Saito"],
+    expanded: false
+  },
+  {
+    id: "24350",
+    sys_id: "59bdc88147b10610d1f9cc39116d43c9",
+    name: "SW Development Division",
+    parent: "24300",
+    department_head: ["Zhengtai Xize"],
+    description:
+      "Develops software and applications for various company needs.",
+    primary_contact: "Zhengtai Xize",
+    members: ["Mai Kato", "Takuya Watanabe"],
+    expanded: false
+  },
+  { /////
+    id: "24300",
+    sys_id: "547d808147b10610d1f9cc39116d433e",
+    name: "System Division",
+    parent: "",
+    department_head: ["曠弌 Sato"],
+    description: "Manages and oversees all system-related operations.",
+    primary_contact: "Ryuhei Masuda",
+    members: ["Aoi Nakamura", "Yuto Kobayashi"],
+    expanded: false
+  },
+  {
+    id: "24900",
+    sys_id: "032656b147d102d10d1f9cc39116d4338",
+    name: "Management Department",
+    parent: "",
+    department_head: ["青地 美智"],
+    description: "Handles overall business management and strategy.",
+    primary_contact: null,
+    members: ["Rina Akiyama", "Kenji Matsuda"],
+    expanded: false
+  },
+  {
+    id: "24351",
+    sys_id: "7dcc08147b10610d1f9cc39116d4348",
+    name: "SW Development Division1G",
+    parent: "24350",
+    department_head: ["朗弘 余之助"],
+    description:
+      "Specializes in the development of 1G related software solutions.",
+    primary_contact: "朗弘 余之助",
+    members: ["Naoki Ishikawa", "Yuki Tanaka"],
+    expanded: false
+  },
+  {
+    id: "24353",
+    sys_id: "8edd0c8147b10610d1f9cc39116d434b",
+    name: "SW Development Division 2G",
+    parent: "24350",
+    department_head: ["Tomoya Kenmochi"],
+    description:
+      "Focuses on 2G software technologies and project implementations.",
+    primary_contact: "Tomoya Kenmochi",
+    members: ["Hikaru Nakamura", "Koichi Ito"],
+    expanded: false
+  },
+  {
+    id: "24355",
+    sys_id: "a1ed488147b10610d1f9cc39116d4362",
+    name: "SW Development Division 3G",
+    parent: "24350",
+    department_head: ["Kohei Shito"],
+    description: "Engaged in the advancement and support of 3G technologies.",
+    primary_contact: "Kohei Shito",
+    members: ["Mana Yoshida", "Daiki Yamamoto"],
+    expanded: false
+  },
+  {
+    id: "24357",
+    sys_id: "65fdc48147b10610d1f9cc39116d4386",
+    name: "SW Development Division 4G",
+    parent: "24350",
+    department_head: ["Satoshi Arita"],
+    description:
+      "Develops 4G technology solutions and supports related projects.",
+    primary_contact: "Satoshi Arita",
+    members: ["Yuji Kojima", "Megumi Fujii"],
+    expanded: false
+  },
+  {
+    id: "24330",
+    sys_id: "1f9d488147b10610d1f9cc39116d435e",
+    name: "System Division",
+    parent: "24300",
+    department_head: ["Satoshi Arita"],
+    description:
+      "Provides critical system maintenance and technology upgrades.",
+    primary_contact: "Ryuhei Masuda",
+    members: ["Takumi Sasaki", "Akane Takahashi"],
+    expanded: false
+  },
+  {
+    id: "24110",
+    sys_id: "7decc08147b10610d1f9cc39116d43aa",
+    name: "Solution Sales Department, Division 1",
+    parent: "24100",
+    department_head: ["Keisuke Hamai"],
+    description:
+      "Manages client relationships and sales strategies for Division 1.",
+    primary_contact: "Keisuke Hamai",
+    members: ["Sakura Inoue", "Masato Fujimoto"],
+    expanded: false
+  },
+  {
+    id: "24111",
+    sys_id: "110d8c4147b10610d1f9cc39116d430f",
+    name: "Solution Sales Department, Division 1 1G",
+    parent: "24110",
+    department_head: ["Sho Shimajiri"],
+    description:
+      "Handles the sales and deployment of 1G technologies within Division 1.",
+    primary_contact: "Sho Shimajiri",
+    members: ["Yui Aragaki", "Keita Suzuki"],
+    expanded: false
+  },
+  {
+    id: "24113",
+    sys_id: "631d408147b10610d1f9cc39116d4367",
+    name: "Solution Sales Department, Division 1 2G",
+    parent: "24110",
+    department_head: ["Sho Shimajiri"],
+    description:
+      "Handles the sales and deployment of 1G technologies within Division 1.",
+    primary_contact: "Sho Shimajiri",
+    members: ["Yui Aragaki", "Keita Suzuki"],
+    expanded: false
+  },
+  {
+    id: "24130",
+    sys_id: "0c3d4c4147b10610d1f9cc39116d4387",
+    name: "Solution Sales Department, Division 2",
+    parent: "24100",
+    department_head: ["Maya Yamada"],
+    description:
+      "Focuses on expanding market presence and managing sales in Division 2.",
+    primary_contact: "Maya Yamada",
+    members: ["Hana Mori", "Takashi Nakamura"],
+    expanded: false
+  },
+  {
+    id: "24131",
+    sys_id: "fa4d808147b10610d1f9cc39116d4338",
+    name: "Solution Sales Department, Division 2 1G",
+    parent: "24130",
+    department_head: ["Keisuke Hamai"],
+    description:
+      "Leads the sales initiatives for 1G products within Division 2.",
+    primary_contact: "Keisuke Hamai",
+    members: ["Riku Tanaka", "Kazuya Yoshida"],
+    expanded: false
+  },
+  {
+    id: "24133",
+    sys_id: "eb5d048147b10610d1f9cc39116d43f0",
+    name: "Solution Sales Department, Division 2 2G",
+    parent: "24130",
+    department_head: ["Kentaro Ito"],
+    description:
+      "Specializes in 2G product sales and customer service for Division 2.",
+    primary_contact: "Kentaro Ito",
+    members: ["Aoi Suzuki", "Ryo Takahashi"],
+    expanded: false
+  },
+  {
+    id: "24100",
+    sys_id: "362616b147d102d10d1f9cc39116d43d8",
+    name: "Sales headquarters",
+    parent: "",
+    department_head: ["Yuichiro Sato"],
+    description:
+      "Oversees all sales operations and strategic planning across the company.",
+    primary_contact: "Yuichiro Sato",
+    members: ["Kaito Yamamoto", "Nao Kojima"],
+    expanded: false
+  },
+  {
+    id: "24103",
+    sys_id: "2bcc408147b10610d1f9cc39116d4378",
+    name: "Business Division, Sales Headquarters",
+    parent: "24100",
+    department_head: ["Yuichiro Sato"],
+    description: "Focuses on business development and strategic partnerships.",
+    primary_contact: "Yuichiro Sato",
+    members: ["Rei Fujita", "Minato Kobayashi"],
+    expanded: false
+  },
+  {
+    id: "24101",
+    sys_id: "86bc4ccd47710610d1f9cc39116d4329",
+    name: "Sales Headquarters (nursing care)",
+    parent: "24100",
+    department_head: ["Yuichiro Sato"],
+    description:
+      "Manages sales strategies and operations within the nursing care sector.",
+    primary_contact: "Yuichiro Sato",
+    members: ["Haruto Watanabe", "Kento Mori"],
+    expanded: false
+  },
+  {
+    id: "24310",
+    sys_id: "6a8d808147b10610d1f9cc39116d4340",
+    name: "Research and Development Division",
+    parent: "24300",
+    department_head: ["Shinichi Haramoto"],
+    description:
+      "Conducts research and develops new technologies and products.",
+    primary_contact: "Shinichi Haramoto",
+    members: ["Daichi Sawamura", "Tetsuya Kuroko"],
+    expanded: false
+  },
+];
+
+
+
+export function buildDepartmentTree(elements, parentId = "") {
+  let branch = [];
+    elements.filter(element => element.parent === parentId).forEach(element => {
+        let children = buildDepartmentTree(elements, element.id);
+        if (children.length) {
+            element.children = children;
+        } else {
+            element.children = [];
+        }
+        branch.push(element);
+    });
+    return branch;
+}
+
+//console.log(JSON.stringify(buildDepartmentTree(departmentData), null, 2))
